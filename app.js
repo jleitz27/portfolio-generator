013e68167b286cb6ab7fecffe68eebc0f3269625
Jason L <jkleitz27@gmail.com>
2022-05-28 20:59:53 -0400

    html page

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
+const fs = require('fs');
 
-// const generatePage = require('./src/page-template');
+const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
+const pageHTML = generatePage(name, github);
 
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if(err) throw err;
+fs.writeFile('./index.html', pageHTML, err => {
+    if(err) throw err;
 
-//     console.log ('Portfolio complete! Check out index.html to see the output!');
-// });
+    console.log ('Portfolio complete! Check out index.html to see the output!');
+});
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -138,6 +138,14 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-    .then(answers => console.log(answers))
     .then(promptProject)
-    .then(projectAnswers => console.log(projectAnswers));
+    .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
+
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if(err) throw err;
+
+            console.log ('Portfolio complete! Check out index.html to see the output!');
+        });
+    });
